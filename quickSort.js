5020dee552f769afc1a8af5e9e5bc57b6a6c5d01
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-04-14 19:51:44 -0500

    quicksort

--- quickSort.js
+++ quickSort.js
@@ -1,7 +1,40 @@
 'use strict';
 
+function swap(array, a, b) {
+  var temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
+
+function partition(array, start, end) {
+  var left, right, pivot,
+    length = end - start;
+
+  if (length < 1) {
+    return;
+  }
+
+  left = start;
+  right = end;
+  pivot = array[start + Math.floor(Math.random() * length) % length];
+
+  while (left < right) {
+    while (array[left] < pivot) {
+      left++;
+    }
+    while (array[right] > pivot) {
+      right--;
+    }
+    swap(array, left, right);
+  }
+
+  partition(array, start, left - 1);
+  partition(array, left + 1, end);
+}
+
 module.exports = {
   quickSort: function(array) {
-    return Array.prototype.sort.call(array, function(a, b) { return a - b; });
+    partition(array, 0, array.length - 1);
+    return array;
   }
 };
--- test/quickSortTest.js
+++ test/quickSortTest.js
@@ -15,6 +15,17 @@ describe('quickSort()', function() {
     assert.deepEqual(actual, [-4, 2, 3, 4, 7, 27, 500, 2020]);
   });
 
+  it('should order the items in an array', function() {
+    // arrange
+    var a = [24, 5, 3, 35, 14, 23, 19, 43, 2];
+
+    // act
+    var actual = main.quickSort(a);
+
+    // assert
+    assert.deepEqual(actual, [2, 3, 5, 14, 19, 23, 24, 35, 43]);
+  });
+
   it('should handle an empty array', function() {
     // arrange
     var a = [];
