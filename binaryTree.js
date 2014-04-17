08d6fdbc3a72e67b7a5c4068ae2e0624476920ac
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-04-16 20:38:56 -0500

    BST

---
+++ binaryTree.js
@@ -0,0 +1,102 @@
+'use strict';
+
+function BinaryTree() {
+  this.root = null;
+}
+
+function Entry(value) {
+  this.left = null;
+  this.right = null;
+  this.value = value;
+}
+
+function insertInSubTree(node, value) {
+  if (value === node.value) {
+    throw new Error('Item already exists in the BST');
+  }
+  else if (value < node.value) {
+    if (node.left === null) {
+      node.left = new Entry(value);
+    }
+    else {
+      insertInSubTree(node.left, value);
+    }
+  }
+  else if (value > node.value) {
+    if (node.right === null) {
+      node.right = new Entry(value);
+    }
+    else {
+      insertInSubTree(node.right, value);
+    }
+  }
+}
+
+BinaryTree.prototype.put = function put(value) {
+  if (this.root === null) {
+    this.root = new Entry(value);
+  }
+  else {
+    insertInSubTree(this.root, value);
+  }
+};
+
+function subTreeContains(node, value) {
+  if (node === null) {
+    return false;
+  }
+  else if (value === node.value) {
+    return true;
+  }
+  else if (value < node.value) {
+    return subTreeContains(node.left, value);
+  }
+  else if (value > node.value) {
+    return subTreeContains(node.right, value);
+  }
+}
+
+BinaryTree.prototype.contains = function contains(value) {
+  return subTreeContains(this.root, value);
+};
+
+function addTreeInOrder(node, array) {
+  if (node === null) return;
+  addTreeInOrder(node.left, array);
+  array.push(node.value);
+  addTreeInOrder(node.right, array);
+}
+
+BinaryTree.prototype.inOrderArray = function() {
+  var array = [];
+  addTreeInOrder(this.root, array);
+  return array;
+};
+
+function addTreePreOrder(node, array) {
+  if (node === null) return;
+  array.push(node.value);
+  addTreePreOrder(node.left, array);
+  addTreePreOrder(node.right, array);
+}
+
+BinaryTree.prototype.preOrderArray = function() {
+  var array = [];
+  addTreePreOrder(this.root, array);
+  return array;
+};
+
+function addTreePostOrder(node, array) {
+  if (node === null) return;
+  addTreePostOrder(node.left, array);
+  addTreePostOrder(node.right, array);
+  array.push(node.value);
+}
+
+BinaryTree.prototype.postOrderArray = function() {
+  var array = [];
+  addTreePostOrder(this.root, array);
+  return array;
+};
+
+module.exports = BinaryTree;
