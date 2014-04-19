a72a2ed50dd804fa74f2c106a9816ce9ba3d4aac
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-04-18 22:56:52 -0500

    permutations

--- recursion.js
+++ recursion.js
@@ -8,6 +8,23 @@ function findMin(array) {
   return Math.min(findMin(array.slice(0, middle)), findMin(array.slice(middle, array.length)));
 }
 
+// iterate through every letter, perform permutations with it added to the prefix plus the permutations of the remaining characters
+function permutations(prefix, string, array) {
+  if (string.length === 0) {
+    array.push(prefix);
+  }
+  else {
+    for (var i = 0; i < string.length; i ++) {
+      permutations(prefix + string.charAt(i), string.substring(0, i) + string.substring(i + 1, string.length), array);
+    }
+  }
+}
+
 module.exports = {
-  findMin: findMin
+  findMin: findMin,
+  permutations: function(str) {
+    var array = [];
+    permutations('', str, array);
+    return array;
+  }
 };
--- test/recursionTest.js
+++ test/recursionTest.js
@@ -6,12 +6,23 @@ var assert        = require('assert'),
 describe('findMin()', function() {
   it('should find the minimum value in the array', function() {
     // arrange
-    var a = [9, 654, 56, 73, 6, 8, 4, 84]
+    var a = [9, 654, 56, 73, 6, 8, 4, 84];
 
     // act
     var actual = recursion.findMin(a);
 
     // assert
     assert.equal(actual, 4);
+  });
+});
+
+describe('permutations()', function() {
+  it('should create an array of all permutations of the string', function() {
+    // arrange
+    // act
+    var actual = recursion.permutations('abc');
+
+    // assert
+    assert.deepEqual(actual, ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']);
   });
 });
