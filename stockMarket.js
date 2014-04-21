ed1a63be3676be3ca3979be68ca3a05d8efb07fa
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-04-20 21:43:51 -0500

    stock market

---
+++ stockMarket.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function bestDealInStockMarket(market) {
+  var deal,
+      buyIndex = 0,
+      bestDeal = 0;
+
+  for (var i = 0; i < market.length; i++) {
+    // keep track of the cheapest price of the stock
+    if (market[i] < market[buyIndex]) {
+      buyIndex = i;
+    }
+
+    // check if today's price minus the previous cheapest price is better than the current best deal
+    if (market[i] - market[buyIndex] > bestDeal) {
+      deal = {
+        buyIndex: buyIndex,
+        sellIndex: i
+      }
+      bestDeal = market[deal.sellIndex] - market[deal.buyIndex];
+    }
+  }
+
+  return deal;
+};
