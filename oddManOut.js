bebaf0643c077f002f6e9d7be2513c7bd8ccc907
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-04-22 21:05:04 -0500

    odd man out

--- oddManOut.js
+++ oddManOut.js
@@ -2,23 +2,33 @@
 
 // also see xorTest
 
-module.exports = function oddManOut(array) {
-  // loop through the array, add each item to a set, if it
-  // doesn't exist in the set add it to the sum, otherwise
-  // delete it from the set and subtract it from the sum
-  var sum = 0,
-      set = {};
+module.exports = {
+  oddManOut: function oddManOut(array) {
+    // loop through the array, add each item to a set, if it
+    // doesn't exist in the set add it to the sum, otherwise
+    // delete it from the set and subtract it from the sum
+    var sum = 0,
+        set = {};
 
-  for (var i = 0; i < array.length; i++) {
-    var val = array[i];
-    if (set.hasOwnProperty(val)) {
-      delete set[val];
-      sum -= val;
+    for (var i = 0; i < array.length; i++) {
+      var val = array[i];
+      if (set.hasOwnProperty(val)) {
+        delete set[val];
+        sum -= val;
+      }
+      else {
+        set[val] = val;
+        sum += val;
+      }
     }
-    else {
-      set[val] = val;
-      sum += val;
+    return sum;
+  },
+  
+  oddManOutXor: function oddManOutXor(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+      sum = sum ^ array[i];
     }
+    return sum;
   }
-  return sum;
 };
--- test/oddManOutTest.js
+++ test/oddManOutTest.js
@@ -9,7 +9,20 @@ describe('oddManOut()', function() {
     var a = [2, 4, 9, 4, 9, 3, 5, 2, 3];
 
     // act
-    var actual = oddManOut(a);
+    var actual = oddManOut.oddManOut(a);
+
+    // assert
+    assert.equal(actual, 5);
+  });
+});
+
+describe('oddManOutXor()', function() {
+  it('should find the odd man out', function() {
+    // arrange
+    var a = [2, 4, 9, 4, 9, 3, 5, 2, 3];
+
+    // act
+    var actual = oddManOut.oddManOutXor(a);
 
     // assert
     assert.equal(actual, 5);
