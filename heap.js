ef1a1663e24f58d21714def1b61f987b5ab90eda
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-04-29 22:10:22 -0500

    heap

--- heap.js
+++ heap.js
@@ -7,39 +7,66 @@ function Heap() {
   this.data = [];
 }
 
+function parentIndex(i) {
+  if (i === 0) {
+    return null;
+  }
+  return Math.floor((i - 1) / 2);
+}
+
+function leftIndex(i, maxLength) {
+  var left = 2 * i + 1;
+  if (typeof maxLength !== 'undefined' && left >= maxLength) {
+    return null;
+  }
+  return left;
+}
+
+function rightIndex(i, maxLength) {
+  var right = 2 * i + 2;
+  if (typeof maxLength !== 'undefined' && right >= maxLength) {
+    return null;
+  }
+  return right;
+}
+
+function swap(array, a, b) {
+  var temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+}
+
 Heap.prototype.insert = function insert(value) {
   var i = this.data.length;
-  var parent = (i === 0) ? 0 : Math.floor((i - 1) / 2);
+  var parent = parentIndex(i);
 
   // insert at the end
   this.data[i] = value;
 
   // then bubble it up till it's greater than all of it's children
-  while (this.data[i] > this.data[parent]) {
+  while (parent !== null && this.data[i] > this.data[parent]) {
     // swap with parent
-    var temp = this.data[parent];
-    this.data[parent] = this.data[i];
-    this.data[i] = temp;
+    swap(this.data, parent, i);
 
     // compute new parent
     i = parent;
-    parent = (i === 0) ? 0 : Math.floor((i - 1) / 2);
+    parent = parentIndex(i);
   }
 };
 
-function addNodesInOrder(result, heap, nodeIndex) {
+function addNodesInOrder(result, heap, current) {
   // left
-  var left = 2 * nodeIndex + 1;
-  if (left < heap.length) {
+  var left = leftIndex(current, heap.length);
+  if (left) {
     addNodesInOrder(result, heap, left);
   }
 
   // this node
-  result.push(heap[nodeIndex]);
+  result.push(heap[current]);
 
   // right
-  var right = 2 * nodeIndex + 2;
-  if (right < heap.length) {
+  var right = rightIndex(current, heap.length);
+  if (right) {
     addNodesInOrder(result, heap, right);
   }
 }
@@ -63,36 +90,34 @@ Heap.prototype.sort = function sort() {
   var length = result.length;
   while (length > 1) {
     // swap the first item with the last item in the heap
-    var temp = result[length - 1];
-    result[length - 1] = result[0];
-    result[0] = temp;
+    swap(result, 0, length - 1);
     length = length - 1;
 
+    // move the first item down until it's a valid heap again
     var current = 0;
     while (true) {
-      var left = 2 * current + 1;
-      var right = 2 * current + 2;
+      var left = leftIndex(current, length);
+      var right = rightIndex(current, length);
 
-      // move the first item down until it's a valid heap again
-      var lessThanLeft = left < length && result[current] < result[left];
-      var lessThanRight = right < length && result[current] < result[right];
-      var leftGreaterThanRight = right >= length || result[left] > result[right];
-      if (lessThanLeft && leftGreaterThanRight) {
-        // swap with the left child
-        temp = result[left];
-        result[left] = result[current];
-        result[current] = temp;
+      if (left === null) {
+        // no children
+        break;
+      }
+
+      if (right === null && result[current] < result[left]) {
+        // left only and greater than left child
+        swap(result, current, left);
         current = left;
       }
-      else if (lessThanRight) {
-        // swap with the right child
-        temp = result[right];
-        result[right] = result[current];
-        result[current] = temp;
-        current = right;
-      }
-      else {
-        break;
+      else if (result[current] < result[left] || result[current] < result[right]) {
+        if (result[left] > result[right]) {
+          swap(result, current, left);
+          current = left;
+        }
+        else {
+          swap(result, current, right);
+          current = right;
+        }
       }
     }
   }
