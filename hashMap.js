ad931063707986b28b5b368bcd08c5775980e95e
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-05-01 20:17:18 -0500

    hash map

---
+++ hashMap.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var hashFunctions = require('./hashFunctions');
+
+function HashMap() {
+}
+
+HashMap.prototype.set = function set(key, value) {
+};
+
+HashMap.prototype.has = function has(key) {
+};
+
+HashMap.prototype.get = function get(key) {
+};
+
+HashMap.prototype.delete = function deleteKey(key) {
+};
+
+module.exports = HashMap;
---
+++ test/hashMapTest.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var assert  = require('assert'),
+    HashMap = require('../hashMap');
+
+describe('HashMap', function() {
+  describe('has(key)', function() {
+    it('should return true if the hash map contains the key', function() {
+      // arrange
+      var hashSet = new HashMap();
+      hashSet.set('hello', 'you');
+
+      // act
+      var actual = hashSet.has('hello');
+
+      // assert
+      assert.equal(actual, true);
+    });
+
+    it('should return false if the hash map does not contain the key', function() {
+      // arrange
+      var hashSet = new HashMap();
+      hashSet.set('hello');
+
+      // act
+      var actual = hashSet.has('hello1');
+
+      // assert
+      assert.equal(actual, false);
+    });
+  });
+
+  describe('get(key)', function() {
+    it('should return the value assigned to the key', function() {
+      // arrange
+      var hashSet = new HashMap();
+      hashSet.set('hello', 'you');
+
+      // act
+      var actual = hashSet.get('hello');
+
+      // assert
+      assert.equal(actual, 'you');
+    });
+
+    it('should return null if no value is assigned to the key', function() {
+      // arrange
+      var hashSet = new HashMap();
+      hashSet.set('hello');
+
+      // act
+      var actual = hashSet.get('hello1');
+
+      // assert
+      assert.equal(actual, null);
+    });
+  });
+
+  describe('set(key, value)', function() {
+    it('should set the value to the key', function() {
+      // arrange
+      var hashSet = new HashMap();
+
+      // act
+      hashSet.set('hello', 'you');
+
+      // assert
+      assert.equal(hashSet.get('hello'), 'you');
+    });
+
+    it('should expand the underlying data structure when the load factor is greater than 0.75', function() {
+      // arrange
+      var hashSet = new HashMap(2);
+
+      // act
+      hashSet.set('hello', 'you');
+      hashSet.set('hello2', 'you2');
+      hashSet.set('hello3', 'you3');
+
+      // assert
+      assert.equal(hashSet.get('hello'), 'you');
+      assert.equal(hashSet.get('hello2'), 'you2');
+      assert.equal(hashSet.get('hello3'), 'you3');
+    });
+  });
+
+  describe('delete(key)', function() {
+    it('should delete the value assigned to the key', function() {
+      // arrange
+      var hashSet = new HashMap();
+      hashSet.set('hello', 'you');
+
+      // act
+      hashSet.delete('hello');
+
+      // assert
+      assert.equal(hashSet.has('hello'), false);
+    });
+  });
+});
